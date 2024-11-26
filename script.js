1b3c548de935093b9dcf46b28ae23fdaa2e0ab8f
miniisami <160603947+miniisami@users.noreply.github.com>
2024-11-26 08:30:04 -0300

    hkjhkj

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ const elemento = document.querySelector('body')
 inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'dark' : 'light'
     elemento.setAttribute("data-bs-theme", modo)
-})const inputCheck = document.querySelector('#modo-noturno')
-const elemento = document.querySelector('body')
-
-inputCheck.addEventListener('click', () => {
-    const modo = inputCheck.checked ? 'dark' : 'light'
-    elemento.setAttribute("data-bs-theme", modo)
 })
 
 function enviarFormulario() {
